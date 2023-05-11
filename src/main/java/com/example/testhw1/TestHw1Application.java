d2a86c303d9e1acb4279c00f90ace1ccd97fadc6
krela <krel.andrej@yandex.ru>
2023-05-11 20:57:30 +0700

    1coom

---
+++ src/main/java/com/example/testhw1/TestHw1Application.java
@@ -0,0 +1,13 @@
+package com.example.testhw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHw1Application.class, args);
+    }
+
+}
